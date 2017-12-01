538bdee4a561d15fc180a1280f7d691670faf1e3
Mark Hibberd <mark@hibberd.id.au>
2017-12-01 12:06:13 +1100

    Transformer style.

--- src/Nest.hs
+++ src/Nest.hs
@@ -24,6 +24,9 @@ module Nest (
   , force
   ) where
 
+
+import           Control.Monad.IO.Class (MonadIO (..))
+
 import           Data.ByteString (ByteString)
 import           Data.String (IsString (..))
 import           Data.Text (Text)
@@ -37,7 +40,6 @@ import qualified Data.Map as Map
 import           Nest.Prelude
 
 import qualified System.Exit as Exit
-import           System.IO (IO)
 import qualified System.IO as IO
 import qualified System.Posix.Env.ByteString as Posix
 
@@ -62,90 +64,94 @@ newtype Environment =
       getEnvironment :: Map ByteString ByteString
     } deriving (Eq, Ord, Show)
 
-newtype Parser a =
+newtype Parser m a =
   Parser {
-       parse :: Environment -> Either NestError a
+       parse :: Environment -> EitherT NestError m a
     }
 
-instance Functor Parser where
+instance Functor m => Functor (Parser m) where
   fmap f m =
     Parser $ \e -> f <$> parse m e
 
-instance Applicative Parser where
+instance Monad m => Applicative (Parser m) where
   pure a =
     Parser $ \_ -> pure a
   f <*> a =
     Parser $ \e ->
       parse f e <*> parse a e
 
-instance Monad Parser where
+instance Monad m => Monad (Parser m) where
   return =
     pure
   m >>= f =
     Parser $ \e ->
-      case parse m e of
-         Left err ->
-           Left err
-         Right a ->
-           parse (f a) e
+      newEitherT $
+        runEitherT (parse m e) >>= \x ->
+          case x of
+            Left err ->
+             pure $ Left err
+            Right a ->
+             runEitherT $ parse (f a) e
 
-option :: Parser a -> Parser (Maybe a)
-option p =
-  Parser $ \e ->
-    case parse p e of
-      Left (NestMissing _) ->
-        Right Nothing
-      Left err ->
-        Left err
-      Right x ->
-        Right (Just x)
-
-withDefault :: Parser a -> a -> Parser a
-withDefault p dfault =
-  Parser $ \e ->
-    case parse p e of
-      Left (NestMissing _) ->
-        Right dfault
-      x ->
-        x
-
-withContext :: Parser a -> Text -> Parser a
-withContext p context =
-  Parser $ \e ->
-    case parse p e of
-      Left err ->
-        Left $ NestContextError context err
-      x ->
-        x
-
-variable :: ByteString -> Parser ByteString
+variable :: Monad m => ByteString -> Parser m ByteString
 variable name =
   Parser $ \e ->
-    fromMaybeM (Left $ NestMissing name) $
+    fromMaybeM (left $ NestMissing name) $
       Map.lookup name $
         getEnvironment e
 
-string :: IsString s => ByteString -> Parser s
+string :: (Monad m, IsString s) => ByteString -> Parser m s
 string name =
   with (variable name) $
     fromString . Text.unpack . Text.decodeUtf8
 
-numeric :: (Read n, Num n) => ByteString -> Parser n
+numeric :: (Monad m, Read n, Num n) => ByteString -> Parser m n
 numeric name = do
   s <- string name
   Parser $ \_ ->
-    fromMaybeM (Left . NestParseError name . mconcat $ ["Could not parse numeric value from '", s , "'"]) $
+    fromMaybeM (left . NestParseError name . mconcat $ ["Could not parse numeric value from '", s ,"'"]) $
       readMaybe . Text.unpack $ s
 
-run :: Parser a -> IO (Either NestError a)
-run p =
-  with Posix.getEnvironment $ \e ->
-    parse p (Environment $ Map.fromList e)
+option :: Monad m => Parser m a -> Parser m (Maybe a)
+option p =
+  Parser $ \e ->
+    newEitherT $ runEitherT (parse p e) >>= \xx -> case xx of
+      Left (NestMissing _) ->
+        pure $ Right Nothing
+      Left err ->
+        pure $ Left err
+      Right x ->
+        pure $ Right (Just x)
 
-force :: Parser a -> IO a
+withDefault :: Monad m => Parser m a -> a -> Parser m a
+withDefault p dfault =
+  Parser $ \e ->
+    newEitherT $ runEitherT (parse p e) >>= \xx ->
+      case xx of
+        Left (NestMissing _) ->
+          pure $ Right dfault
+        x ->
+          pure x
+
+withContext :: Monad m => Parser m a -> Text -> Parser m a
+withContext p context =
+  Parser $ \e ->
+    newEitherT $ runEitherT (parse p e) >>= \xx ->
+      case xx of
+        Left err ->
+          pure . Left $ NestContextError context err
+        x ->
+          pure x
+
+run :: MonadIO m => Parser m a -> m (Either NestError a)
+run p =
+  liftIO Posix.getEnvironment >>= \e ->
+    runEitherT (parse p (Environment $ Map.fromList e))
+
+force :: MonadIO m => Parser m a -> m a
 force p =
   run p >>= \e -> case e of
-    Left err -> do
+    Left err -> liftIO $ do
       Text.hPutStrLn IO.stderr $ renderNestError err
       Exit.exitFailure
     Right a ->
